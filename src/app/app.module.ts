b4a8c00c070bc9c03db15bcf3e507bee6b7e131b
saurabh <saurabhmagar840@gmail.com>
2021-03-01 16:03:00 +0530

    added ngdecorator and import ngmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,11 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+    providers: [BrowserModule],
+    declarations: [],
+    bootstrap: [AppModule]
+})
+
+
 export class AppModule {}
