5c0051ba628c9b0082bb3716166e0ed0e8504b64
saurabh <saurabhmagar840@gmail.com>
2021-03-01 16:36:25 +0530

    component added ngComponent and import component

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello</h1>',
+    styles: [`
+        h1 {background-color: 'white'; }`
+    ],
+})
+
+
+
+export class AppComponent{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-    providers: [BrowserModule],
+    imports: [BrowserModule],
     declarations: [],
     bootstrap: [AppModule]
 })
